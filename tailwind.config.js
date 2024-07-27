0305fd357d103f7a225131ed77cdb8eff1a5095b
Vinayak Aggarwal <vinayakaggarwal05@gmail.com>
2024-07-27 21:06:42 +0530

    basic set up

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{js,jsx,ts,tsx}'],
+	theme: {
+		extend: {}
+	},
+	plugins: [],
+	// needed to add this to load all type of lineclamp so that we can use it dynamically
+	safelist: [
+		{
+			pattern: /^line-clamp-(\d+)$/
+		},
+		{
+			pattern: /^max-w-(\d+)$/
+		}
+	]
+};
