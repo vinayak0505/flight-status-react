6b91744c90c183440fe4885a4879dd278a2e80d3
Vinayak Aggarwal <vinayakaggarwal05@gmail.com>
2024-07-30 19:11:44 +0530

    Updates

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,36 @@
+// Scripts for firebase and firebase messaging
+importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js");
+importScripts(
+  "https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js"
+);
+
+// Initialize the Firebase app in the service worker
+// "Default" Firebase configuration (prevents errors)
+const firebaseConfig = {
+  apiKey: "AIzaSyBAn0qds2ongfFpLAPbIs6pVsIr5V3Mf8Y",
+  authDomain: "test-e37ab.firebaseapp.com",
+  projectId: "test-e37ab",
+  storageBucket: "test-e37ab.appspot.com",
+  messagingSenderId: "301151428688",
+  appId: "1:301151428688:web:2f4486b2636ebcaf1cf155"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+// Retrieve firebase messaging
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  if(payload?.data?.type === "ALLFLIGHT"
+  || payload?.data?.type === "ALLGATE"
+  ) {
+    return;
+  }
+  const notificationTitle =  payload?.data?.title ?? payload?.notification?.title;
+  const notificationOptions = {
+    body: payload?.data?.body ?? payload?.notification?.body,
+    icon: payload?.notification?.image ?? null,
+  };
+  
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
